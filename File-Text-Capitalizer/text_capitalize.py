9137130f6741391d353766e9003304d129b31842
DGrothe-PhD <[dag_fs@t-online.de]>
2022-10-30 13:22:34 +0100

    make overwriting optional, simplify path syntax by os.path.join, example added

--- File-Text-Capitalizer/text_capitalize.py
+++ File-Text-Capitalizer/text_capitalize.py
@@ -4,29 +4,36 @@
 import os
 
 
-def bulk_rename(path):
+def bulk_rename(path, overwrite=True):
     files = os.listdir(path)
     for user_file in files:
         if os.path.splitext(user_file)[1] == ".txt":
             capitalize_list = []
 
             # first reading txt line and captializing it and saving sentences in list
-            with open(f"{path}{user_file}", "r+") as f:
-                j = f.readlines()
+            user_file_path = os.path.join(path, user_file)
+            with open(user_file_path, "r+") as f_in:
+                j = f_in.readlines()
                 for line in j:
                     capitalize_line = line.capitalize()
                     capitalize_list.append(capitalize_line)
-
+            f_in.close()
+            
+            if overwrite:
+                out_file_path = user_file_path
+            else:
+                out_file_path = os.path.splitext(user_file)[0] + "_new"+ os.path.splitext(user_file)[1]
             # resetting txt file so that we can append our capitalize txt file
-            with open(f"{path}{user_file}", "w") as f:
-                f.write("")
+            with open(out_file_path, "w") as f_out:
+                f_out.write("")
 
             # writing capitalize sentences into txt file
-            with open(f"{path}{user_file}", "a") as f:
+            with open(out_file_path, "a") as f_out:
                 for sentence in capitalize_list:
-                    f.write(sentence)
+                    f_out.write(sentence)
 
 
 if __name__ == '__main__':
     path = input("Enter the path of txt files: ")
-    bulk_rename(path)
+    ow = input("Overwrite existing files? Type Yes")
+    bulk_rename(path, ow == "Yes")
